85d84032efad761d2863cac0b6c7b8bde7c6bd83
akhileshyadav26 <shailesh1416@gmail.com>
2022-09-12 13:28:17 +0530

    Commited final project

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, 
+        setGoal, 
+        updateGoal, 
+        deleteGoal
+    } = require('../controllers/goalController')
+
+const {protect} = require('../middleware/authMiddleware')
+
+// router.get('/',getGoals)
+// router.post('/',setGoals)
+router.route('/').get(protect,getGoals).post(protect,setGoal)
+
+// router.put('/:id',updateGoal)
+// router.delete('/:id',deleteGoal)
+router.route('/:id').delete(protect,deleteGoal).put(protect,updateGoal)
+
+module.exports = router
